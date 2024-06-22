b72c8d21c733e5d807c80be2d5dbf0d517823b16
Acer <kkl24@mail.ru>
2024-06-22 19:55:38 +0300

    fixup! +Override annotations

--- dz_13_06/src/Programmer.java
+++ dz_13_06/src/Programmer.java
@@ -4,10 +4,12 @@ public class Programmer extends Worker{
     Programmer(String name, String lastName){
         super(name,lastName,"Programmer");
     }
+    @Override
     public void goToWork(){
         super.goToWork();
         System.out.println("Writing a program");
     }
+    @Override
     public void goToVacation(int days){
         super.goToVacation(days);
         System.out.println("Отдых 1");
